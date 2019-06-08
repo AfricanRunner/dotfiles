21f47254da2b3533695290b1680575755a812d9c
Daniël du Preez <dupreezdaniel8@gmail.com>
2019-06-08 09:58:21 -0500

    Added copy script

---
+++ copy.sh
@@ -0,0 +1,11 @@
+
+echo "Copying vimrc..."
+cp .vimrc ~/.vimrc
+
+echo "Copying tmux.conf..."
+cp .tmux.conf ~/.tmux.conf
+
+echo "Copying .zshrc..."
+cp .zshrc ~/.zshrc
+
+echo "Finished copying dotfiles!"
